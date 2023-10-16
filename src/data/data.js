bb7f2ce8e4a38084ae887abecd4c573a56866186
Ayush-Dabas <aydbs23@gmail.com>
2023-10-16 20:15:20 +0530

    added react text utils

--- src/data/data.js
+++ src/data/data.js
@@ -1,33 +1,34 @@
 import WorkIm from "../assets/projects/workImg.jpeg";
-import RealEst from "../assets/projects/realestate.jpg";
+// import RealEst from "../assets/projects/realestate.jpg";
+import ReactTextUtils from "../assets/projects/react-text-utils.png";
 
 export const data = [
   {
     id: 1,
-    name: "React JS Application",
+    name: "Nike E commerce",
     image: WorkIm,
     github: "",
     live: "",
   },
   {
     id: 2,
-    name: "React JS Application",
-    image: RealEst,
-    github: "",
-    live: "",
+    name: "React text utils",
+    image: ReactTextUtils,
+    github: "https://github.com/Ayush-Dabas/react-text-utils",
+    live: "https://ayush-dabas.github.io/react-text-utils/",
   },
-  {
-    id: 3,
-    name: "React JS Application",
-    image: WorkIm,
-    github: "",
-    live: "",
-  },
-  {
-    id: 4,
-    name: "React JS Application",
-    image: RealEst,
-    github: "",
-    live: "",
-  },
+  // {
+  //   id: 3,
+  //   name: "React JS Application",
+  //   image: WorkIm,
+  //   github: "",
+  //   live: "",
+  // },
+  // {
+  //   id: 4,
+  //   name: "React JS Application",
+  //   image: RealEst,
+  //   github: "",
+  //   live: "",
+  // },
 ];
